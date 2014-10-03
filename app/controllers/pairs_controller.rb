c0f81cc098b99c6b2b7111f2190446b4f35d380a
sturnquest <sturnquest@gmail.com>
2014-10-02 23:36:54 -0700

    $1

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -3,11 +3,7 @@ class PairsController < ApplicationController
   def show
     @@match_maker ||= MatchMaker.seed(JSON.load(IO.read(File.join(Rails.root, 'config', 'organization.json'))))
 
-    logger.info("emails: #{@@match_maker.emails.inspect}")
-
     pairs = @@match_maker.pairs(params[:id].to_i)
-
-    logger.info("pairs: #{pairs.inspect}")
 
     render :json => pairs, :content_type => 'application/json'
   end
